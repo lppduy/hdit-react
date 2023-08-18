9c1b1c075698a07447e504d5235c35a80e6f219e
lppduy <lephamphuongduy@gmail.com>
2023-08-18 07:33:59 +0700

    video-16

---
+++ src/components/MyComponents.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class MyComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        duy ne
+        {25 * 300}
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
