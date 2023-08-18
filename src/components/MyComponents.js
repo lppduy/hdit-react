c6866dc92b7525ae9339f251fdf111c3e06a0032
lppduy <lephamphuongduy@gmail.com>
2023-08-18 08:25:59 +0700

    video 19

--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -1,11 +1,29 @@
 import React from 'react';
 
 class MyComponent extends React.Component {
+  state = {
+    name: 'EREN YEEGER',
+    age: 26,
+    address: 'sg',
+  };
+
+  handleClick(e) {
+    console.log('click rui!!');
+    // console.log(e);
+    // console.log('my name is', this, state.name); // error
+  }
+
+  handleOnMouseOver(e) {
+    console.log(e.pageX);
+  }
+
   render() {
     return (
       <div>
-        duy ne
-        {25 * 300}
+        My name is {this.state.name} in {this.state.address} and i'm{' '}
+        {this.state.age}
+        <button onMouseOver={this.handleOnMouseOver}>Hover me ~</button>
+        <button onClick={this.handleClick}>Click me ~</button>
       </div>
     );
   }
