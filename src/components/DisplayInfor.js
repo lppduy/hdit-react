b499b86bd8c42c5e6d6c362c8029653ca6c8b747
lppduy <lephamphuongduy@gmail.com>
2023-08-19 15:23:17 +0700

    v25

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,35 +1,53 @@
 import React from 'react';
 
 class DisplayInfor extends React.Component {
+  state = {
+    isShowListUser: true,
+  };
+
+  handleShowHide = () => {
+    this.setState({
+      isShowListUser: !this.state.isShowListUser,
+    });
+  };
+
   render() {
     // destructuring
     // const { age, name } = this.props;
     const { listUsers } = this.props;
-    console.log(listUsers);
+    // console.log(listUsers);
     // = const listUsers = this.props.listUsers
 
     // console.log(this.props);
     return (
       <div>
-        {listUsers.map(user => {
-          console.log(user);
-          // dung index coi chung dinh bugs
-          return (
-            <div key={user.id}>
-              <div>My name {user.name}</div>
-              <div>My age {user.age}</div>
-            </div>
-          );
-        })}
-        {/* <div>My name is {name}</div>
-        <div>My age is {age}</div>
-        <hr />
-        <div>My name is {name}</div>
-        <div>My age is {age}</div>
-        <hr />
-        <div>My name is {name}</div>
-        <div>My age is {age}</div>
-        <hr /> */}
+        <div>
+          <span
+            onClick={() => {
+              this.handleShowHide();
+            }}
+          >
+            {this.state.isShowListUser
+              ? ' Hide list users:'
+              : ' Show list users:'}
+          </span>
+        </div>
+        {this.state.isShowListUser && (
+          <div>
+            {listUsers.map(user => {
+              console.log('>>> check map user', user);
+              // dung index coi chung dinh bugs
+
+              return (
+                <div key={user.id} className={+user.age > 18 ? 'green' : 'red'}>
+                  <div>My name {user.name}</div>
+                  <div>My age {user.age}</div>
+                  <hr />
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     );
   }
