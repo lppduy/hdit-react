e7c969fb088ae10e96ff90a5739c5084fe5f6105
lppduy <lephamphuongduy@gmail.com>
2023-08-21 14:30:56 +0700

    functional component started

--- src/App.js
+++ src/App.js
@@ -1,38 +1,11 @@
 // import logo from './logo.svg';
-import './App.scss';
+// import './App.scss';
 // import { useDispatch, useSelector } from 'react-redux';
 // import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 import MyComponent from './components/MyComponents';
 import React from 'react';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="app-container">
-        <h1>Duy Kingdom</h1>
-        <MyComponent />
-      </div>
-    );
-  }
-}
-
-// const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
-//   );
-
+const App = () => {
+  return <div className="App">Duy Kingdom</div>;
+};
 export default App;
