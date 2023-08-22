a406a1f4c811d6a0b3e3be7a63121b49ba7b3efe
lppduy <lephamphuongduy@gmail.com>
2023-08-22 08:47:52 +0700

    v48

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.scss';
 import Header from './components/Header/Header';
+import { Link } from 'react-router-dom';
+
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <div>
+        test link
+        <div>
+          <button>
+            <Link to="/users">Go to user page</Link>
+          </button>
+          <button>
+            <Link to="/admins">Go to admin page</Link>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
