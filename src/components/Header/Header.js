e95efbebb62b3c8844d7dfdb1ed4a76d939da0b7
lppduy <lephamphuongduy@gmail.com>
2023-08-23 14:42:08 +0700

    v51 - index routes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,10 @@ const Header = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Duy Kingdom</Navbar.Brand>
+        {/* <Navbar.Brand href="#home">Duy Hub</Navbar.Brand> */}
+        <NavLink to="/" className="navbar-brand">
+          Duy Hub
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
@@ -27,9 +30,9 @@ const Header = () => {
           </Nav>
           <Nav>
             <NavDropdown title="Settings" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Logout</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Profile</NavDropdown.Item>
+              <NavDropdown.Item>Login</NavDropdown.Item>
+              <NavDropdown.Item>Logout</NavDropdown.Item>
+              <NavDropdown.Item>Profile</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
