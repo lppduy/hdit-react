ebb8f1007ad72d3ff10b4897549f5eaa5ada5e2e
lppduy <lephamphuongduy@gmail.com>
2023-08-25 17:15:53 +0700

    v53

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -6,6 +6,19 @@ function HomePage(props) {
       <video autoPlay muted loop>
         <source src={videoHompage} type="video/mp4 " />
       </video>
+
+      <div className="homepage-content">
+        <div className="title-1">There's a better way to ask</div>
+        <div className="title-2">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore amet,
+          iste eius cupiditate eum veniam voluptatem rerum repudiandae
+          exercitationem nobis ratione iusto illo laboriosam harum inventore.
+          Odio iusto sequi quia!
+        </div>
+        <div className="title-3">
+          <button>Get's started. It's free</button>
+        </div>
+      </div>
     </div>
   );
 }
