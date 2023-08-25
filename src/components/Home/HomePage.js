898ac17c726f93f690ae2e84cd33c841b1aa21e6
lppduy <lephamphuongduy@gmail.com>
2023-08-25 16:51:50 +0700

    video 52

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,5 +1,13 @@
+import videoHompage from '../../assets/video-homepage.mp4';
+
 function HomePage(props) {
-  return <div className="homepage-container">home page component</div>;
+  return (
+    <div className="homepage-container">
+      <video autoPlay muted loop>
+        <source src={videoHompage} type="video/mp4 " />
+      </video>
+    </div>
+  );
 }
 
 export default HomePage;
