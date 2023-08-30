662001b55ac45c4fa684f40d388b5b757b27a987
lppduy <lephamphuongduy@gmail.com>
2023-08-30 10:01:42 +0700

    v57

--- src/components/Admin/Content/ModalCreateUser.js
+++ src/components/Admin/Content/ModalCreateUser.js
@@ -2,12 +2,29 @@ import React from 'react';
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import { FcPlus } from 'react-icons/fc';
 
 const ModalCreateUser = () => {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [role, setRole] = useState('USER');
+  const [image, setImage] = useState('');
+  const [previewImage, setPreviewImage] = useState(null);
+
+  const handleUploadImage = e => {
+    if (e.target && e.target.files && e.target.files[0]) {
+      setPreviewImage(URL.createObjectURL(e.target.files[0]));
+      setImage(e.target.files[0]);
+    }
+
+    console.log('>>> upload file', e.target?.files[0]);
+  };
 
   return (
     <>
@@ -23,6 +40,7 @@ const ModalCreateUser = () => {
         onHide={handleClose}
         size="xl"
         backdrop="static"
+        className="modal-add-user"
       >
         <Modal.Header closeButton>
           <Modal.Title>Add new user</Modal.Title>
@@ -34,6 +52,8 @@ const ModalCreateUser = () => {
               <input
                 type="email"
                 className="form-control"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
               />
             </div>
             <div className="col-md-6">
@@ -41,6 +61,8 @@ const ModalCreateUser = () => {
               <input
                 type="password"
                 className="form-control"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
               />
             </div>
 
@@ -49,23 +71,46 @@ const ModalCreateUser = () => {
               <input
                 type="text"
                 className="form-control"
+                value={username}
+                onChange={e => setUsername(e.target.value)}
               />
             </div>
             <div className="col-md-4">
               <label className="form-label">Role</label>
-              <select className="form-select">
-                <option
-                  selected
-                  value="USER"
-                >
-                  USER
-                </option>
+              <select
+                className="form-select"
+                onChange={e => setRole(e.target.value)}
+                value={role}
+              >
+                <option value="USER">USER</option>
                 <option value="ADMIN">ADMIN</option>
               </select>
             </div>
+
             <div className="col-md-12">
-              <label className="form-label">Image</label>
-              <input type="file" />
+              <label
+                className="form-label label-upload"
+                htmlFor="labelUpload"
+              >
+                <FcPlus />
+                Upload File Image
+              </label>
+              <input
+                type="file"
+                id="labelUpload"
+                hidden
+                onChange={e => handleUploadImage(e)}
+              />
+            </div>
+            <div className="col-md-12 img-preview">
+              {previewImage ? (
+                <img
+                  src={previewImage}
+                  alt="img-preview"
+                />
+              ) : (
+                <span>Preview Image</span>
+              )}
             </div>
           </form>
         </Modal.Body>
